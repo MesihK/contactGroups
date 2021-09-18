39cf0abbc323eb733238f5f44abdc70088029fec
Mesih Kilinc <mesihkilinc@gmail.com>
2021-09-17 19:13:19 -0500

    add msa transformer contact map evaluation and multiple dca evaluation

--- utils_mesih.py
+++ utils_mesih.py
@@ -12,6 +12,41 @@ from utils_pfammsa import pfammsa
 colorscheme1 = ['#a93a28', '#afc8cd', '#266674', '#fb8c32', '#cbc96d',
 '#60e6c1', '#d7295e', '#008ed0', '#747474']
 
+def evaldisttrans(args):
+    distfile = args[0] # A 6 A 7 0.123 resid
+    contacts = args[1]  # 0 1 0.123     index
+    threshold = float(args[2])
+
+    # load files
+    dist = cp.loadtuplesregex(distfile, '\s{1,}')
+    cnt = np.loadtxt(contacts, delimiter=',')
+
+    # clasify distance as contact '1' or no contact '0' based on threshold
+    distScore = [ 1 if float(e) <= threshold else 0 for a,b,c,d,e in dist ]
+
+    cntScore = list()
+    #I think the first token is the start token, so we ignore it
+    for i in range(1,len(cnt[0])):
+        for j in range(i+1,len(cnt[0])):
+            cntScore.append(cnt[i][j])
+
+
+    # calculate the ROC curve
+    aucScore = roc_auc_score(distScore, cntScore)
+    curve = metrics.roc_curve(distScore, cntScore, pos_label=1)
+    plt.figure(1)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(curve[0], curve[1], color=colorscheme1[0], label='AUC:'+"{:.4f}".format(aucScore))
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+    plt.savefig('roc_curve.png')
+    #plt.show()
+
+    # calculate the ROC area under the curve
+    print('AUC:',"{:.4f}".format(aucScore))
+
 def evaldistdca(args):
     mapfile = args[0]  # 6 K 0 K       resid to alignedIndex
     distfile = args[1] # A 6 A 7 0.123 resid
@@ -57,6 +92,40 @@ def evaldistdca(args):
 
     # calculate the ROC area under the curve
     print('AUC:',"{:.4f}".format(aucScore))
+
+def evalmultdistdca(args):
+    distfile = args[0] # A 6 A 7 0.123 resid
+    dcafile = args[1].split(',')  # 0 1 0.123     index
+    labels = args[2].split(',')  
+    threshold = float(args[3])
+    
+    colors = ['r','g','b','c','m','y','k']
+
+    # load files
+    dist = cp.loadtuplesregex(distfile, '\s{1,}')
+    dca = [ cp.loadtuplesregex(i, '\s{1,}') for i in dcafile ]
+
+    # clasify distance as contact '1' or no contact '0' based on threshold
+    distScore = [ 1 if float(e) <= threshold else 0 for a,b,c,d,e in dist ]
+    dcaScore = list()
+    for di in dca:
+        dcaScore.append([ float(c) for a,b,c in di ])
+
+    # calculate the ROC curve
+    # calculate the ROC area under the curve
+    plt.figure(1)
+    for i in range(len(dca)):
+        aucScore = roc_auc_score(distScore, dcaScore[i])
+        curve = metrics.roc_curve(distScore, dcaScore[i], pos_label=1)
+        plt.plot(curve[0], curve[1], color=colors[i], label=labels[i]+':'+"{:.4f}".format(aucScore))
+        print('AUC'+str(i)+':',"{:.4f}".format(aucScore))
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+    plt.savefig('roc_curve.png')
+    #plt.show()
 
 # Cluster given MSA with similaritiy cutoff, and then sample each cluster until given threshold achieved 
 def sampleclusters(args):

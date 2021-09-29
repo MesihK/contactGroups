a344502d4d4ee507e57b29d3be44e3af9d605308
Mesih Kilinc <mesihkilinc@gmail.com>
2021-09-29 12:19:52 -0500

    update dca mi contact prediction script

--- script_pfamDCA.sh
+++ script_pfamDCA.sh
@@ -3,89 +3,153 @@ RED='\033[0;31m'
 NC='\033[0m' 
 
 echo 'This script compares given PDB file with MSA made DCA calculations'
-echo ''
 
-if [ $# -ne 4 ]; then
-    echo 'usage: eval.sh pfam.file prot.pdb subs.mat clustThreshold'
-    echo '   ex: eval.sh PF18-3.pfam 1shg.pdb protsub.mat 0.70'
+if [ $# -lt 3 ]; then
+    echo 'usage: eval.sh pfam.file prot.pdb subs.mat gapopen gapextention'
+    echo '   ex: eval.sh PF18-3.pfam 1shg.pdb protsub.mat 10 1'
     exit 0
 fi
 
 PFAM=$1
 PROT=$2
 MAT=$3
-CLUST=$4
+echo -e ${RED}Matrix: $MAT ${NC}
+
+if [ -z $4 ]; then
+    GAPOPEN=10
+else
+    GAPOPEN=$4
+    echo -e ${RED}Gap open: $GAPOPEN ${NC}
+fi
+
+if [ -z $5 ]; then
+    GAPEXT=1
+else
+    GAPEXT=$5
+    echo -e ${RED}Gap extend: $GAPEXT ${NC}
+fi
 SIM=0.7
+DIR=$PFAM.$PROT.$MAT.$GAPOPEN.$GAPEXT
+
+if [ -d $DIR ]; then
+    echo -e ${RED}The result directory: $DIR exists.$NC
+    exit 0
+fi
 
 # remove gaps from MSA file
-echo -e ${RED}sed remove gaps: ${NC}
-sed 's/\.//g' $PFAM > $PFAM.ng
+if [ ! -f $PFAM.ng ]; then 
+    echo -e ${RED}sed remove gaps: ${NC}
+    sed 's/\.//g' $PFAM > $PFAM.ng
+fi
 
 # get protein fasta file
-echo -e ${RED}python utils_protein2.py writeseqfa $PROT: ${NC}
-python utils_protein2.py writeseqfa $PROT
+if [ ! -s $PROT.fa ]; then
+    echo -e ${RED}python utils_protein2.py writeseqfa $PROT: ${NC}
+    python utils_protein2.py writeseqfa $PROT
+fi
 
 # paste the protein fasta to the end of the MSA file
-cat $PROT.fa >> $PFAM.ng
-echo -e ${RED}sed uppercase $PFAM.ng.up: ${NC}
-sed -e 's/\(.*\)/\U\1/' $PFAM.ng > $PFAM.ng.up
+if [ ! -s $PFAM.ng.up ]; then
+    echo -e '\n' >> $PFAM.ng
+    cat $PROT.fa >> $PFAM.ng
+    echo -e ${RED}sed uppercase $PFAM.ng.up: ${NC}
+    sed -e 's/\(.*\)/\U\1/' $PFAM.ng > $PFAM.ng.up
+fi
 
 # Get the name of protein from fasta file, and convert it to uppercase
 NAME=$(cat $PROT.fa | head -n1 | cut -c 2- | sed -e 's/\(.*\)/\U\1/')
 
 
 # Recalculate MSA with muscle
-if [ ! -f $PFAM.$MAT.msa ]; then
-    #echo -e ${RED}muscle -in $PFAM.ng.up -out $PFAM.$MAT.msa -matrix protsub.mat -gapopen -12.0 -gapextend -1.0 -center 0.0: ${NC}
-    #muscle -in $PFAM.ng.up -out $PFAM.$MAT.msa -matrix $MAT -gapopen -6.0 -gapextend -0.5 -center 0.0
-    #muscle -in $PFAM.ng.up -out $PFAM.ng.up.bl -matrix blosum62.mat -gapopen -12.0 -gapextend -1.0 -center 0.0
-    mafft --textmatrix $MAT --thread 8 $PFAM.ng.up > $PFAM.$MAT.msa
+if [ ! -s $PFAM.$MAT.msa ]; then
+    
+    echo -e ${RED}mafft --op $GAPOPEN --ep $GAPEXT --textmatrix $MAT --thread 8 --quiet $PFAM.ng.up '>' $PFAM.$MAT.msa ${NC}
+    #mafft --op $GAPOPEN --ep $GAPEXT --textmatrix $MAT --thread 8 --quiet $PFAM.ng.up > $PFAM.$MAT.msa
+    mafft --op $GAPOPEN --ep $GAPEXT --textmatrix $MAT --thread 8 $PFAM.ng.up > $PFAM.$MAT.msa
+    if [ $? -ne 0 ]; then
+        echo -e ${RED} MSA failed $NC
+        exit 1
+    fi
 else
     echo -e ${RED}Previous MSA found, use it.$NC
 fi
 
 # Get our protein alignment from MSA
 echo -e ${RED}python utils_pfammsa.py getsinglemsa $PFAM.$MAT.msa $NAME $PFAM.$PROT: ${NC}
-python utils_pfammsa.py getsinglemsa $PFAM.$MAT.msa $NAME $PFAM.$PROT.pb
+python utils_pfammsa.py getsinglemsa $PFAM.$MAT.msa $NAME $PFAM.$PROT
 
 # Calculate map file between the PDB and aligned sequence 
-echo -e ${RED}python utils_resimap.py posmapvec4 $PROT.fa $PFAM.${PROT}.pb_MSA.fa $PROT $PROT.pb.map: ${NC}
-python utils_resimap.py posmapvec4 $PROT.fa $PFAM.${PROT}.pb_MSA.fa $PROT $PROT.pb.map
-
-# Cluster the result of MSA with given threshold
-echo -e ${RED}python utils_pfammsa.py getsinglemsacluster $PFAM.$MAT.msa $PROT.pb.map $NAME aa 0.7 $PFAM.$PROT: ${NC}
-python utils_pfammsa.py getsinglemsacluster $PFAM.$MAT.msa $PROT.pb.map $NAME aa $CLUST $PFAM.$PROT
-
-#echo -e ${RED}python utils_pfammsa.py msareduce_withmap $PFAM.$MAT.msa $PROT.pb.map aa $PFAM.$PROT.aa: ${NC}
-#python utils_pfammsa.py msareduce_withmap $PFAM.$MAT.msa $PROT.pb.map aa $PFAM.$PROT.aa
+echo -e ${RED}python utils_resimap.py posmapvec4 $PROT.fa $PFAM.${PROT}_MSA.fa $PROT $PROT.map: ${NC}
+python utils_resimap.py posmapvec4 $PROT.fa $PFAM.${PROT}_MSA.fa $PROT $PROT.map
 
 # Calculate score by reducing msa
-echo -e ${RED}python utils_pfammsa.py msareduce_withmap $PFAM.${PROT}_${CLUST}_cluster.fa $PROT.pb.map aa $PFAM.$PROT.aa: ${NC}
-python utils_pfammsa.py msareduce_withmap $PFAM.${PROT}_${CLUST}_cluster.fa $PROT.pb.map aa $PFAM.$PROT.aa
+echo -e ${RED}python utils_pfammsa.py msareduce_withmap $PFAM.$MAT.msa $PROT.map aa $PFAM.$PROT.aa: ${NC}
+python utils_pfammsa.py msareduce_withmap $PFAM.$MAT.msa $PROT.map aa $PFAM.$PROT.aa
 
 # Calculate hammingweight
 echo -e ${RED}python proc_hammingweight.py $PFAM.$PROT.aa.scoremat $SIM '>' $PROT.weight: ${NC}
-python proc_hammingweight.py $PFAM.$PROT.aa.scoremat $SIM > $PROT.weight
+python proc_hammingweight.py $PFAM.$PROT.aa.scoremat $SIM > $PROT.hamm.weight
 
 # Convert the weight from [1, ... 3] to 1 ... 3
-sed 's/\[//g' $PROT.weight > $PROT.weight1
-sed 's/\]//g' $PROT.weight1 > $PROT.weight2
-sed 's/,//g' $PROT.weight2 > $PROT.weight
-rm $PROT.weight1 $PROT.weight2
+weight_convert () {
+    sed 's/\[//g' $1 > ${1}.1
+    sed 's/\]//g' ${1}.1 > ${1}.2
+    sed 's/,//g' ${1}.2 > $1
+    rm ${1}.1 ${1}.2
+}
+
+weight_convert $PROT.hamm.weight
 
 # Calculate DCA
-echo -e ${RED}python utils_dca.py foo $PFAM.$PROT.aa.scoremat $PROT.weight $PFAM.pydca: ${NC}
-python utils_dca.py dca $PFAM.$PROT.aa.scoremat $PFAM.$PROT.aa.rcol $PROT.weight $PFAM.pydca
+echo -e ${RED}python utils_dca.py foo $PFAM.$PROT.aa.scoremat $PROT.hamm.weight $PFAM.pydca: ${NC}
+python utils_dca.py dca $PFAM.$PROT.aa.scoremat $PFAM.$PROT.aa.rcol $PROT.hamm.weight $PFAM.pydca
 
 # Calculate residue distance using PDB file
 echo -e ${RED}python utils_protein2.py writeresdists $PROT $PROT.dist : ${NC}
 python utils_protein2.py writeresdists $PROT $PROT.dist 
 
 # Compare DCA and DIST and calculate RMSD 
-echo -e ${RED}python utils_mesih.py evaldistdca $PROT.pb.map $PROT.dist $PFAM.pydca: ${NC}
-python utils_mesih.py evaldistdca $PROT.pb.map $PROT.dist $PFAM.pydca 5
+echo -e ${RED}python utils_mesih.py evaldistdca $PROT.map $PROT.dist $PFAM.pydca: ${NC}
+python utils_mesih.py evaldistdca $PROT.map $PROT.dist $PFAM.pydca 5
 
-mv roc_curve.png $PROT.$MAT.$CLUST.roc.png
+mv roc_curve.png hamm.png
 
-#rm $PFAM.ng $PFAM.ng.up $PROT.fa $PFAM.$MAT.msa $PFAM.${PROT}* $PROT.weight # $PROT.map 
+# Calculate dummyweight
+echo -e ${RED}python proc_hammingweight.py $PFAM.$PROT.aa.scoremat $SIM '>' $PROT.weight: ${NC}
+python utils_mesih.py dummyweight $PFAM.$PROT.aa.scoremat > $PROT.dummy.weight
+
+weight_convert $PROT.dummy.weight
+
+# Calculate DCA
+echo -e ${RED}python utils_dca.py foo $PFAM.$PROT.aa.scoremat $PROT.dummy.weight $PFAM.pydca: ${NC}
+python utils_dca.py dca $PFAM.$PROT.aa.scoremat $PFAM.$PROT.aa.rcol $PROT.dummy.weight $PFAM.pydca
+
+# Compare DCA and DIST and calculate RMSD 
+echo -e ${RED}python utils_mesih.py evaldistdca $PROT.map $PROT.dist $PFAM.pydca: ${NC}
+python utils_mesih.py evaldistdca $PROT.map $PROT.dist $PFAM.pydca 5
+
+mv roc_curve.png dummy.png
+
+# Calculate clusterweight
+echo -e ${RED}python proc_hammingweight.py $PFAM.$PROT.aa.scoremat $SIM '>' $PROT.weight: ${NC}
+python utils_mesih.py clusterweight $PFAM.$PROT.aa.scoremat > $PROT.clust.weight
+
+weight_convert $PROT.clust.weight
+
+# Calculate DCA
+echo -e ${RED}python utils_dca.py foo $PFAM.$PROT.aa.scoremat $PROT.clust.weight $PFAM.pydca: ${NC}
+python utils_dca.py dca $PFAM.$PROT.aa.scoremat $PFAM.$PROT.aa.rcol $PROT.clust.weight $PFAM.pydca
+
+# Compare DCA and DIST and calculate RMSD 
+echo -e ${RED}python utils_mesih.py evaldistdca $PROT.map $PROT.dist $PFAM.pydca: ${NC}
+python utils_mesih.py evaldistdca $PROT.map $PROT.dist $PFAM.pydca 5
+
+mv roc_curve.png clust.png
+
+mkdir $DIR
+
+#mv roc_curve.png $DIR/$PROT.$MAT.$CLUST.roc.png
+#mv $PROT.fa $PFAM.$MAT.msa $PFAM.${PROT}_* $PFAM.$PROT.aa* $PROT.weight $PROT.map $PROT.dist $PFAM.pydca $DIR
+mv $PROT.fa $PFAM.$MAT.msa $PFAM.${PROT}_* $PFAM.$PROT.aa* *.weight $PROT.map $PFAM.pydca *.png $DIR
+echo ./eval.sh $@ > $DIR/cmd
 
